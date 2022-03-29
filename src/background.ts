8a78b974fcc4c4456d5fe69fbde6c61e41a687e1
momo3038 <morgan.leroi@gmail.com>
2022-03-29 14:01:52 +0200

    feat: Add Startup and Installed handler

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,12 @@
 import browser = require('webextension-polyfill');
+
+browser.runtime.onInstalled.addListener(onInstalled)
+browser.runtime.onStartup.addListener(onStartup);
+
+function onInstalled() {
+    console.log("Hello Devoxx & App Installed !");
+}
+
+function onStartup() {
+    console.log("Hello Devoxx & App Started !");
+}
